4282a744aa4ac5c928366d81288eaa10f5061001
Nic Yeeles <n.yeeles@gmail.com>
2014-06-26 16:09:28 +0100

    Added delete functionality. Currently deletes all posts

--- time.rb
+++ time.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
 
 get '/' do
-  response['Access-Control-Allow-Origin'] = '*'
-  sleep 2
-  Time.now.to_s
+  # response['Access-Control-Allow-Origin'] = '*'
+  erb :github
+  # sleep 2
+  # Time.now.to_s
 end
